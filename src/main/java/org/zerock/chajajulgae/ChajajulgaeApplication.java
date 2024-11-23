83237fe886331d4339e9f40757923af8a482b8c3
shseo232 <shseo232@gmail.com>
2024-11-23 13:03:39 +0900

    @CommitFirst - Spring Boot 환경설정

---
+++ src/main/java/org/zerock/chajajulgae/ChajajulgaeApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.chajajulgae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChajajulgaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChajajulgaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zerock/chajajulgae/ChajajulgaeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.chajajulgae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChajajulgaeApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
